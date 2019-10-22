6f33dc4d684785ff16f1ad7b05cf395cd9804901
future-sawada <s.sawada.c6@future.co.jp>
2019-10-22 16:15:36 +0900

    addd cube material

---
+++ Assets/Goalkeeper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goalkeeper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // transformを取得
+        Transform myTransform = this.transform;
+
+        // 座標を取得
+        Vector3 pos = myTransform.position;
+        pos.x += 0.01f;
+        pos.y += 0.01f;
+        myTransform.position = pos;  // 座標を設定
+    }
+}
